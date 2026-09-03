4309818b54643f5bf0d6299da894a80400c933d7
Hamdan Yuwafi <hamdayuwafii@gmail.com>
2026-09-03 21:20:00 +0700

    optimasi

--- blog/footer.py
+++ blog/footer.py
@@ -1,11 +1,11 @@
 FOOTER_INFO_LINKS = [
     {"title": "Tentang Kami", "url": "blog:page", "slug": "about"},
-    {"title": "Kirim Karya", "url": "blog:contact"},
+    {"title": "Contact", "url": "blog:contact"},
     {"title": "Kebijakan Privasi", "url": "blog:page", "slug": "privacy"},
 ]
 
 FOOTER_CONFIG = {
-    "brand_fallback_name": "SonicVibe",
+    "brand_fallback_name": "Gala Sagala",
     "brand_fallback_description": "Kurasi visual dan narasi dari kancah musik elektronik, indie, dan festival terpanas di seluruh dunia.",
     "copyright_text": "Overlapping the noise.",
     "newsletter_placeholder": "Email lo...",
